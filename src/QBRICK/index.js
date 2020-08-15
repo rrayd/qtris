b2b35c50e9f2d80714223398c59e5273a2cba422
Dmitrii Raikov <nakovaina89@gmail.com>
2020-08-15 21:52:39 +0500

    update Lines to Drei

--- src/QBRICK/index.js
+++ src/QBRICK/index.js
@@ -1,90 +1,89 @@
-import * as THREE from 'three'
-import { Canvas, extend, useThree, useFrame } from 'react-three-fiber'
 import React, { useRef, useState } from 'react'
-import { OrbitControls } from 'drei'
+import { Canvas, extend, useThree, useFrame } from 'react-three-fiber'
+import { OrbitControls, Line } from 'drei'
+import styled from 'styled-components'
 
+/**
+ * Conf the game
+ */
 const QB_CONF = {
     side: 8, // int
-    height: 8, // int
+    height: 13, // int
 }
 
-const PlayGround = () => {
+/**
+ * Play Ground, target label 0
+ */
+const PGround = () => {
     const sz = QB_CONF.side
     const ground = useRef()
-
-    const perimeter_vertices = [
-        new THREE.Vector3(0, 0, 0),
-        new THREE.Vector3(0, sz, 0),
-        new THREE.Vector3(sz, sz, 0),
-        new THREE.Vector3(sz, 0, 0),
-    ]
 
     const PGrid = () => {
         const lines = []
         for (let i = 1; i <= sz - 1; i++) {
-            lines.push([new THREE.Vector3(0, i, 0), new THREE.Vector3(sz, i, 0)])
-            lines.push([new THREE.Vector3(i, 0, 0), new THREE.Vector3(i, sz, 0)])
+            lines.push([
+                [0, 0, i],
+                [sz, 0, i],
+            ])
+            lines.push([
+                [i, 0, 0],
+                [i, 0, sz],
+            ])
         }
-        return lines.map((vec, index) => (
-            <line key={index}>
-                <geometry attach="geometry" vertices={vec} />
-                <lineBasicMaterial attach="material" color="white" />
-            </line>
-        ))
+        return lines.map((vec3, index) => <Line key={index} points={vec3} color="#0F4C81" linewidth={1} />)
+    }
+
+    const Perimeter = () => {
+        const lines = [
+            [
+                [0, 0, 0],
+                [0, 0, sz],
+            ],
+            [
+                [0, 0, sz],
+                [sz, 0, sz],
+            ],
+            [
+                [sz, 0, sz],
+                [sz, 0, 0],
+            ],
+            [
+                [sz, 0, 0],
+                [0, 0, 0],
+            ],
+        ]
+        return lines.map((vec3, index) => <Line key={index} points={vec3} color="#0F4C81" linewidth={1} />)
     }
 
     return (
         <group ref={ground}>
-            <lineLoop>
-                <geometry attach="geometry" vertices={perimeter_vertices} />
-                <lineBasicMaterial attach="material" color="white" />
-            </lineLoop>
+            <Perimeter />
             <PGrid />
         </group>
     )
 }
 
-function Box(props) {
-    // This reference will give us direct access to the mesh
-    const mesh = useRef()
-
-    // Set up state for the hovered and active state
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-
-    // Rotate mesh every frame, this is outside of React without overhead
-    useFrame(() => (mesh.current.rotation.x = mesh.current.rotation.y += 0.01))
-
-    return (
-        <mesh
-            {...props}
-            ref={mesh}
-            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-            onClick={(e) => setActive(!active)}
-            onPointerOver={(e) => setHover(true)}
-            onPointerOut={(e) => setHover(false)}>
-            <boxBufferGeometry attach="geometry" args={[1, 1, 1]} />
-            <meshStandardMaterial attach="material" color={hovered ? '#0BDCFF' : '#0F4C81'} />
-        </mesh>
-    )
-}
-
 export default function QBRICK() {
     return (
-        <Canvas
-            orthographic
-            camera={{
-                zoom: 50,
-                position: [0, 180, 100],
-            }}
-            style={{ background: '#19140E' }}
-            colorManagement>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
-            <PlayGround position={[0, 0, 0]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
-            <OrbitControls />
-        </Canvas>
+        <THREETRIS>
+            <Canvas
+                orthographic
+                camera={{
+                    zoom: 50,
+                    position: [200, 100, 200],
+                }}
+                style={{ background: '#19140E' }}
+                colorManagement>
+                <ambientLight />
+                <pointLight position={[10, 10, 10]} />
+                <PGround />
+                <OrbitControls />
+            </Canvas>
+        </THREETRIS>
     )
 }
+
+const THREETRIS = styled.div`
+    width: 100%;
+    height: 100%;
+`
