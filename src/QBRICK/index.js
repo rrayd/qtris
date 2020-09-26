121f8b2ecfdc32e81e6003d2a95898e0c198259f
Dmitrii Raikov <nakovaina89@gmail.com>
2020-09-26 22:35:15 +0500

    QVox figures

--- src/QBRICK/index.js
+++ src/QBRICK/index.js
@@ -1,8 +1,15 @@
 import * as THREE from 'three'
-import React, { useRef, useEffect, useState } from 'react'
+import React, {
+    useRef,
+    useEffect,
+    useState,
+    useUpdate,
+    forwardRef,
+} from 'react'
 import { Canvas } from 'react-three-fiber'
-import { Line } from 'drei'
+import { Line, OrbitControls } from 'drei'
 import styled from 'styled-components'
+import { BufferGeometry, Geometry, ShapeBufferGeometry } from 'three'
 
 /**
  * Conf the
@@ -46,6 +53,8 @@ const randomRangeInt = (min, max) => {
  */
 const QGround = () => {
     const Grid = () => {
+        console.count('QGround.Grid')
+
         const lines = []
         for (let i = sc; i <= sz * sc - 1 * sc; i += sc) {
             lines.push([
@@ -63,6 +72,8 @@ const QGround = () => {
     }
 
     const Perimeter = () => {
+        console.count('QGround.Perimeter')
+
         const lines = [
             [
                 [0, 0, 0],
@@ -96,30 +107,11 @@ const QGround = () => {
 }
 
 /**
- * One cell reference
- */
-const QVox = (props) => {
-    const vox = useRef()
-    const [hovered, setHover] = useState(false)
-    return (
-        <mesh
-            {...props}
-            ref={vox}
-            onPointerOver={(e) => setHover(true)}
-            onPointerOut={(e) => setHover(false)}>
-            <boxBufferGeometry attach="geometry" args={[sc, sc, sc]} />
-            <meshStandardMaterial
-                attach="material"
-                color={hovered ? '#0BDCFF' : '#0BDCFF'}
-            />
-        </mesh>
-    )
-}
-
-/**
  * Figures
  */
 const QFigureSource = (name) => {
+    console.count('QFigureSource')
+
     const QF = ['I', 'O', 'S', 'Z', 'L', 'J', 'T'][randomRangeInt(0, 6)]
     switch (name || QF) {
         case 'I':
@@ -161,35 +153,49 @@ const QFigureSource = (name) => {
     }
 }
 
-/**
- * Figure Vector Representation
- */
-const FVR = (source) => ({
-    width: source[0].length,
-    height: source.length,
-    depth: 1,
-    get data() {
-        const substrate = new Uint8Array(this.width * this.height * this.depth)
-        const flatSource = source.reduce((acc, val) => acc.concat(val), [])
-        for (let i = 0; i < flatSource.length; i++) {
-            substrate[i] = flatSource[i]
-        }
-        return substrate
-    },
-    get texture() {
-        return new THREE.DataTexture2DArray(
-            this.data,
-            this.width,
-            this.height,
-            this.depth
-        )
-    },
-})
+// /**
+//  * Figure Vector Representation
+//  */
+// const FVR = (source) => ({
+//     width: source[0].length,
+//     height: source.length,
+//     depth: 1,
+//     get data() {
+//         const substrate = new Uint8Array(this.width * this.height * this.depth)
+//         const flatSource = source.reduce((acc, val) => acc.concat(val), [])
+//         for (let i = 0; i < flatSource.length; i++) {
+//             substrate[i] = flatSource[i]
+//         }
+//         return substrate
+//     },
+//     get texture() {
+//         return new THREE.DataTexture2DArray(
+//             this.data,
+//             this.width,
+//             this.height,
+//             this.depth
+//         )
+//     },
+// })
 
 /**
- * Figure
+ * One cell reference
  */
-const QFigure = ({ source }) => {
+const QVox = () => {
+    console.count('QVox')
+    return <boxBufferGeometry attach="geometry" args={[sc, sc, sc]} />
+    // return <planeBufferGeometry attach="geometry" args={[sc, sc]} />
+    // return new THREE.PlaneGeometry([sc, sc])
+}
+
+/**
+ * Vox Tetris Figures with bounding troubles
+ */
+const QFigure = (props) => {
+    console.count('QFigure')
+
+    const { source, position } = props
+
     const flatSource = source.reduce((acc, val) => acc.concat(val), [])
     const voxels = []
     for (let i = 0, x = 0, y = 0; i < flatSource.length; i++) {
@@ -203,36 +209,143 @@ const QFigure = ({ source }) => {
             x = 0
         }
     }
-    return voxels.map((vec3, i) => (
-        <QVox key={i} position={[vec3[0] + sc / 2, sc / 2, vec3[2] + sc / 2]} />
+
+    // const fixedMesh = (self) => {
+    //     const size = new THREE.Vector3()
+    //     self.geometry.computeBoundingBox()
+    //     self.geometry.boundingBox.getSize(size)
+    //     self.updateMatrixWorld(true)
+    // }
+
+    return voxels.map((vox, i) => (
+        <mesh
+            key={i}
+            // onUpdate={fixedMesh}
+            rotation={[-1.5708, 0, 0]}
+            position={vox}>
+            <QVox />
+            <meshStandardMaterial attach="material" color="#0BDCFF" />
+        </mesh>
     ))
+}
+
+/**
+ * Classic Shape Tetris Figures
+ */
+const QFigureShape = ({ source }) => {
+    console.count('QFigureShape')
+
+    const flatSource = source.reduce((acc, val) => acc.concat(val), [])
+
+    // TODO memo
+    const cellPoints = []
+    for (let i = 0, x = 0, y = 0; i < flatSource.length; i++) {
+        if (flatSource[i]) {
+            cellPoints.push([x, 0, y])
+        }
+        if (x < source[0].length - 1) {
+            x += sc
+        } else {
+            y += sc
+            x = 0
+        }
+    }
+
+    // TODO memo
+    const shape = new THREE.Shape()
+    shape.autoClose = true
+    shape.currentPoint = new THREE.Vector2(0, 0)
+    shape.lineTo(1, 0)
+    shape.lineTo(2, 0)
+    shape.lineTo(2, 2)
+    // cellPoints.forEach((vec3) => {
+    //     !before
+    //         ? shape.quadraticCurveTo(0, 0, vec3[0], vec3[2])
+    //         : shape.quadraticCurveTo(before[0], before[2], vec3[0], vec3[2])
+    //     before = vec3
+    // })
+    const extrudeSettings = {
+        depth: 8,
+        bevelEnabled: true,
+        bevelSegments: 2,
+        steps: 2,
+        bevelSize: 1,
+        bevelThickness: 1,
+    }
+
+    return (
+        <mesh>
+            <shapeBufferGeometry
+                attach={"geometry"}
+                args={shape}
+                extrudeSettings={extrudeSettings}
+                side={THREE.DoubleSide}
+            />
+            <meshPhongMaterial attach="material" color="#0BDCFF" />
+        </mesh>
+    )
 }
 
 /**
  * Action Figure layer
  */
-const QFigureLayer = ({ source }) => {
-    const [figureVector, setFigureVector] = useState(FVR(source))
-    const [layerVector, setLayerVector] = useState()
+const QFigureLayer = (props) => {
+    console.count('QFigureLayer')
+
+    const { source } = props
+
+    const figureRef = useRef()
+
+    const [position, setPosition] = useState([
+        randomRangeInt(0, sz - 1) * sc,
+        0,
+        randomRangeInt(0, sz - 1) * sc,
+    ])
+
+    // const [rotation, setRoration] = useState([-1,5708, 0, 0])
 
     const keyDownHandler = (e) => {
         console.log(e)
     }
 
     useEffect(() => {
+        console.count('QFigureLayer.useEffect[0]')
+
         window.addEventListener('keydown', keyDownHandler)
         return () => {
             window.removeEventListener('keydown', keyDownHandler)
         }
     }, [0])
 
-    return <QFigure source={source} />
+    useEffect(() => {
+        console.count('QFigureLayer.useEffect[position, rotation]')
+
+        // const boundingBox = new THREE.Box3()
+        // const mesh = figureRef.current.children[0]
+        // boundingBox.copy(mesh.geometry.boundingBox)
+        // mesh.updateMatrixWorld(true)
+        // boundingBox.applyMatrix4(mesh.matrixWorld)
+    }, [position])
+
+    return (
+        <group
+            ref={figureRef}
+            position={[
+                position[0] + sc / 2,
+                position[1] + sc / 2,
+                position[2] + sc / 2,
+            ]}>
+            <QFigure source={source} />
+        </group>
+    )
 }
 
 /**
  * Cursor
  */
 const QActiveLabel = ({ playing }) => {
+    console.count('QActiveLabel')
+
     const [label, nextLabel] = useState(sh)
     const [source, newSource] = useState(QFigureSource())
 
@@ -252,9 +365,6 @@ const QActiveLabel = ({ playing }) => {
             interval = null
         }
     }
-
-    const elementLayer = useRef()
-
     return <QFigureLayer source={source} />
 }
 
@@ -262,6 +372,8 @@ const QActiveLabel = ({ playing }) => {
  * Runtime
  */
 export default function QBRICK() {
+    console.count('QBRICK')
+
     return (
         <THREETRIS>
             <Canvas
@@ -276,6 +388,7 @@ export default function QBRICK() {
                 <pointLight position={[-200, 50, 200]} />
                 <QGround />
                 <QActiveLabel />
+                <OrbitControls />
             </Canvas>
         </THREETRIS>
     )
